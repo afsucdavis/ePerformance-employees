3e1a90847f977e5e83a8dfb7b5b3ab378b3bd88c
AFSUCDAVIS <afsucdavis@gmail.com>
2020-02-24 09:29:55 -0800

    new files
    new files

---
+++ story_content/6LYiOWSAsoF_44100_48_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.37.21453.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.082%0D%0AIn%20the%20left%20menu,%20click%20the%20Performance%20Workcenter.%20%0D%0A%0D%0A"}')
